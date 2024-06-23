15e30f34d764f42edc696859c61e373a67f409b4
ysy-255 <ysy-255@outlook.com>
2024-06-23 15:56:36 +0900

    関数の追加

--- code/IMAGE.hpp
+++ code/IMAGE.hpp
@@ -10,10 +10,10 @@ struct IMAGE{
 	std::vector<std::vector<unsigned char>>R;
 	std::vector<std::vector<unsigned char>>G;
 	std::vector<std::vector<unsigned char>>B;
-	unsigned int width;
 	unsigned int height;
+	unsigned int width;
 	IMAGE() = default;
-	IMAGE(unsigned int width, unsigned int height):
+	IMAGE(unsigned int height, unsigned int width):
 		width(width),
 		height(height),
 		R(height, std::vector<unsigned char>(width)),
@@ -23,37 +23,63 @@ struct IMAGE{
 };
 
 float image_compare(const IMAGE &image1, const IMAGE &image2){
-	long long gap =0;
-	for(int i =0; i < image1.height; i++){
-		for(int j =0; j < image1.width; j++){
+	long long gap = 0;
+	for(int i = 0; i < image1.height; i++){
+		for(int j = 0; j < image1.width; j++){
 			gap += std::pow(std::abs(image1.R[i][j] - image2.R[i][j]), 2);
 			gap += std::pow(std::abs(image1.G[i][j] - image2.G[i][j]), 2);
 			gap += std::pow(std::abs(image1.B[i][j] - image2.B[i][j]), 2);
 		}
 	}
-	return std::pow(gap / (image1.width * image1.height * 3), 0.5);
+	return std::pow(gap / (image1.height * image1.width * 3), 0.5);
 }
 
-void image2imos(IMAGE &image, IMAGE &reserve){
-	int R,G,B;
-	for(int i = image.height -1; i >=0; i--){
-		for(int j = image.width -1; j >=0; j--){
-			R = (image.R[i][j] - (i >0 ? image.R[i -1][j] : 0)) - ((j >0 ? image.R[i][j -1] : 0) - (i >0 && j >0 ? image.R[i -1][j -1] : 0));
-			G = (image.G[i][j] - (i >0 ? image.G[i -1][j] : 0)) - ((j >0 ? image.G[i][j -1] : 0) - (i >0 && j >0 ? image.G[i -1][j -1] : 0));
-			B = (image.B[i][j] - (i >0 ? image.B[i -1][j] : 0)) - ((j >0 ? image.B[i][j -1] : 0) - (i >0 && j >0 ? image.B[i -1][j -1] : 0));
-			R >=0 ? R *= 2 : R = -R * 2 -1;
-			G >=0 ? G *= 2 : G = -G * 2 -1;
-			B >=0 ? B *= 2 : B = -B * 2 -1;
-			image.R[i][j] = R % 256 ;
-			image.G[i][j] = G % 256 ;
-			image.B[i][j] = B % 256 ;
-			reserve.R[i][j] = R >> 8;
-			reserve.G[i][j] = G >> 8;
-			reserve.B[i][j] = B >> 8;
+void image2imos(IMAGE & image){
+	for(int y = image.height -1; y >= 0; y--){
+		for(int x = image.width -1; x >= 0; x--){
+			image.R[y][x] += (y > 0 && x > 0 ? image.R[y - 1][x - 1] : 0) - (y > 0 ? image.R[y - 1][x] : 0) - (x > 0 ? image.R[y][x - 1] : 0);
+			image.G[y][x] += (y > 0 && x > 0 ? image.G[y - 1][x - 1] : 0) - (y > 0 ? image.G[y - 1][x] : 0) - (x > 0 ? image.G[y][x - 1] : 0);
+			image.B[y][x] += (y > 0 && x > 0 ? image.B[y - 1][x - 1] : 0) - (y > 0 ? image.B[y - 1][x] : 0) - (x > 0 ? image.B[y][x - 1] : 0);
 		}
 	}
 	return;
 }
 
+void imos2image(IMAGE & image){
+	for(int y = 0; y < image.height; y++){
+		for(int x = 0; x < image.width; x++){
+			image.R[y][x] -= (y > 0 && x > 0 ? image.R[y - 1][x - 1] : 0) - (y > 0 ? image.R[y - 1][x] : 0) - (x > 0 ? image.R[y][x - 1] : 0);
+			image.G[y][x] -= (y > 0 && x > 0 ? image.G[y - 1][x - 1] : 0) - (y > 0 ? image.G[y - 1][x] : 0) - (x > 0 ? image.G[y][x - 1] : 0);
+			image.B[y][x] -= (y > 0 && x > 0 ? image.B[y - 1][x - 1] : 0) - (y > 0 ? image.B[y - 1][x] : 0) - (x > 0 ? image.B[y][x - 1] : 0);
+		}
+	}
+	return;
+}
+
+#define NEAREST_NEIGHBOR 0
+
+IMAGE image_enlarge(IMAGE & image, float scaleY, float scaleX, unsigned char type){
+	if(image.height * scaleY > 65535 || image.height * scaleY > 65535){
+		char c;
+		std::cerr << "too big image size!\ncontinue?[Y/n]";
+		if(c != 'y' && c != 'Y')return {};
+	}
+	IMAGE result(static_cast<int>(image.height * scaleY), static_cast<int>(image.width * scaleX));
+	switch(type){
+		case NEAREST_NEIGHBOR:{
+			for(int y = 0; y < result.height; y++){
+				for(int x = 0; x < result.width; x++){
+					int ypoint = static_cast<int>((2 * y + 1) / scaleY) / 2;
+					int xpoint = static_cast<int>((2 * x + 1) / scaleX) / 2;
+					result.R[y][x] = image.R[ypoint][xpoint];
+					result.G[y][x] = image.G[ypoint][xpoint];
+					result.B[y][x] = image.B[ypoint][xpoint];
+				}
+			}
+			break;
+		}
+	}
+	return result;
+}
 
 #endif
