446cf3025af509d2dcbdc77a10c74d080fa573f1
ysy-255 <ysy-255@outlook.com>
2024-06-23 12:18:30 +0900

    一部修正、出力する関数を追加

--- code/PNG.hpp
+++ code/PNG.hpp
@@ -24,26 +24,13 @@ struct PNG{
 	std::vector<unsigned char> methods;
 };
 
-unsigned char PaethPredictor(unsigned char a /* left */,unsigned char b/* above */,unsigned char c/* upper left */){
-    unsigned char p = a + b - c;
-
-    // pa = |b - c|　　　横向きの値の変わり具合
-    // pb = |a - c|　　　縦向きの値の変わり具合
-    // pc = |b-c + a-c|　↑ふたつの合計
-    unsigned char pa = abs(p - a);    
-    unsigned char pb = abs(p - b);    
-    unsigned char pc = abs(p - c);    
-
-    // 横向きのほうがなだらかな値の変化 → 左
-    if (pa <= pb && pa <= pc)
-        return a;
-
-    // 縦向きのほうがなだらかな値の変化 → 上
-    if (pb <= pc)
-        return b;
-        
-    // 縦横それぞれ正反対に値が変化するため中間色を選択 → 左上        
-    return c;
+unsigned char PaethPredictor(unsigned char a /* left */, unsigned char b /* above */, unsigned char c /* upper left */){
+	unsigned short pa = abs(b - c);
+	unsigned short pb = abs(a - c);
+	unsigned short pc = abs(a + b - c - c);
+	if(pa <= pb && pa <= pc) return a;
+	if(pb <= pc) return b;
+	return c;
 }
 
 void PNGstream2img(const std::vector<unsigned char> & datastream, IMAGE & img, std::vector<unsigned char> & methods, const unsigned int & width, const unsigned int & height, const unsigned char & color){
@@ -78,9 +65,9 @@ void PNGstream2img(const std::vector<unsigned char> & datastream, IMAGE & img, s
 			case 2:
 				if(y == 0){
 					for(unsigned int x = 0; x < width; x++){
-						img.R[y][x] = datastream[offset ++];
-						img.G[y][x] = datastream[offset ++];
-						img.B[y][x] = datastream[offset ++];
+						img.R[0][x] = datastream[offset ++];
+						img.G[0][x] = datastream[offset ++];
+						img.B[0][x] = datastream[offset ++];
 						if(color == 4)offset ++;
 					}
 				}
@@ -95,14 +82,14 @@ void PNGstream2img(const std::vector<unsigned char> & datastream, IMAGE & img, s
 				break;
 			case 3:
 				if(y == 0){
-					img.R[y][0] = datastream[offset ++];
-					img.G[y][0] = datastream[offset ++];
-					img.B[y][0] = datastream[offset ++];
+					img.R[0][0] = datastream[offset ++];
+					img.G[0][0] = datastream[offset ++];
+					img.B[0][0] = datastream[offset ++];
 					if(color == 4)offset ++;
 					for(unsigned int x = 1; x < width; x++){
-						img.R[y][x] = datastream[offset ++] + img.R[y][x - 1] / 2;
-						img.G[y][x] = datastream[offset ++] + img.G[y][x - 1] / 2;
-						img.B[y][x] = datastream[offset ++] + img.B[y][x - 1] / 2;
+						img.R[0][x] = datastream[offset ++] + img.R[0][x - 1] / 2;
+						img.G[0][x] = datastream[offset ++] + img.G[0][x - 1] / 2;
+						img.B[0][x] = datastream[offset ++] + img.B[0][x - 1] / 2;
 						if(color == 4)offset ++;
 					}
 				}
@@ -121,14 +108,14 @@ void PNGstream2img(const std::vector<unsigned char> & datastream, IMAGE & img, s
 				break;
 			case 4:
 				if(y == 0){
-					img.R[y][0] = datastream[offset ++];
-					img.G[y][0] = datastream[offset ++];
-					img.B[y][0] = datastream[offset ++];
+					img.R[0][0] = datastream[offset ++];
+					img.G[0][0] = datastream[offset ++];
+					img.B[0][0] = datastream[offset ++];
 					if(color == 4)offset ++;
 					for(unsigned int x = 1; x < width; x++){
-						img.R[y][x] = datastream[offset ++] + PaethPredictor(img.R[y][x - 1], 0, 0);
-						img.G[y][x] = datastream[offset ++] + PaethPredictor(img.G[y][x - 1], 0, 0);
-						img.B[y][x] = datastream[offset ++] + PaethPredictor(img.B[y][x - 1], 0, 0);
+						img.R[0][x] = datastream[offset ++] + PaethPredictor(img.R[0][x - 1], 0, 0);
+						img.G[0][x] = datastream[offset ++] + PaethPredictor(img.G[0][x - 1], 0, 0);
+						img.B[0][x] = datastream[offset ++] + PaethPredictor(img.B[0][x - 1], 0, 0);
 						if(color == 4)offset ++;
 					}
 				}
@@ -138,9 +125,9 @@ void PNGstream2img(const std::vector<unsigned char> & datastream, IMAGE & img, s
 					img.B[y][0] = datastream[offset ++] + PaethPredictor(0, img.B[y - 1][0], 0);
 					if(color == 4)offset ++;
 					for(unsigned int x = 1; x < width; x++){
-						img.R[y][x] = datastream[offset ++] + PaethPredictor(img.R[y][x - 1], img.R[y - 1][0], img.R[y - 1][x - 1]);
-						img.G[y][x] = datastream[offset ++] + PaethPredictor(img.G[y][x - 1], img.G[y - 1][0], img.G[y - 1][x - 1]);
-						img.B[y][x] = datastream[offset ++] + PaethPredictor(img.B[y][x - 1], img.B[y - 1][0], img.B[y - 1][x - 1]);
+						img.R[y][x] = datastream[offset ++] + PaethPredictor(img.R[y][x - 1], img.R[y - 1][x], img.R[y - 1][x - 1]);
+						img.G[y][x] = datastream[offset ++] + PaethPredictor(img.G[y][x - 1], img.G[y - 1][x], img.G[y - 1][x - 1]);
+						img.B[y][x] = datastream[offset ++] + PaethPredictor(img.B[y][x - 1], img.B[y - 1][x], img.B[y - 1][x - 1]);
 						if(color == 4)offset ++;
 					}
 				}
@@ -174,7 +161,7 @@ void readPNG(
 	unsigned char color;
 
 	std::vector<unsigned char> compressed_data;
-	std::vector<unsigned char> filtered_data;
+	std::vector<unsigned char> datastream;
 	do{
 		length = UV4_UI(FileData, offset, false);
 		offset += 4;
@@ -197,7 +184,7 @@ void readPNG(
 					color = 4;
 					break;
 			}
-			filtered_data.resize((PNGimage.Width + 1) * PNGimage.Height * color);
+			datastream.resize((PNGimage.Width * color + 1) * PNGimage.Height);
 		}
 		else if(type == "IDAT"){
 			size_t currentSize = compressed_data.size();
@@ -209,7 +196,7 @@ void readPNG(
 			offset += length;
 		}
 		offset += 4;
-		std::cerr << type << ":" << length << ";" << std::endl;
+		// std::cerr << type << ":" << length << ";" << std::endl;
 	} while(type != "IEND");
 
 	z_stream z;
@@ -222,8 +209,8 @@ void readPNG(
 	}
 	z.next_in = compressed_data.data();
 	z.avail_in = compressed_data.size();
-	z.next_out = filtered_data.data();
-	z.avail_out = filtered_data.size();
+	z.next_out = datastream.data();
+	z.avail_out = datastream.size();
 	int ret;
 	do{
 		ret = inflate(&z, Z_FINISH);
@@ -235,7 +222,74 @@ void readPNG(
 		}
 	} while(ret != Z_STREAM_END);
 	inflateEnd(&z);
-	PNGstream2img(filtered_data, PNGimage.ImageData, PNGimage.methods, PNGimage.Width, PNGimage.Height, color);
+	PNGstream2img(datastream, PNGimage.ImageData, PNGimage.methods, PNGimage.Width, PNGimage.Height, color);
+	return;
+}
+
+void writePNG(
+	const IMAGE ImageData,
+	const std::string outPath,
+	std::vector<unsigned char> methods
+	){
+	bool AdditionalFilter = false;
+	for(unsigned char & uc : methods) AdditionalFilter |= (uc > 4);
+	std::ofstream out(outPath, std::ios::binary);
+	out << (unsigned char)137 << (unsigned char)80 << (unsigned char)78 << (unsigned char)71
+	    << (unsigned char) 13 << (unsigned char)10 << (unsigned char)26 << (unsigned char)10;
+	UI_write(13, out, false);
+	out << "IHDR";
+	UI_write(ImageData.width, out, false);
+	UI_write(ImageData.height, out, false);
+	out << (unsigned char)8;
+	out << (unsigned char)2;
+	out << (unsigned char)0;
+	out << (unsigned char)AdditionalFilter; // Filter Method の研究のため他と区別
+	out << (unsigned char)0;
+	UI_write(0, out, false); // CRC分からん
+
+	std::vector<unsigned char> datastream((ImageData.width * 3 + 1) * ImageData.height);
+	std::vector<unsigned char> compressed_data((ImageData.width * 3 + 1) * ImageData.height);
+	int offset = 0;
+	for(int h = 0; h < ImageData.height; h++){
+		datastream[offset ++] = methods[h];
+		for(int w = 0; w < ImageData.width; w++){
+			datastream[offset ++] = ImageData.R[h][w];
+			datastream[offset ++] = ImageData.G[h][w];
+			datastream[offset ++] = ImageData.B[h][w];
+		}
+	}
+	z_stream z;
+	z.zalloc = Z_NULL;
+	z.zfree = Z_NULL;
+	z.opaque = Z_NULL;
+	if(deflateInit(&z, Z_DEFAULT_COMPRESSION) != Z_OK){
+		std::cerr << "z_streamの初期化に失敗" << std::endl;
+		return;
+	}
+	z.next_in = datastream.data();
+	z.avail_in = datastream.size();
+	z.next_out = compressed_data.data();
+	z.avail_out = compressed_data.size();
+	int ret;
+	do{
+		ret = deflate(&z, Z_FINISH);
+		if(ret == Z_STREAM_END)break;
+		if(ret != Z_OK){
+			std::cerr << "圧縮処理に失敗" << std::endl;
+			deflateEnd(&z);
+			return;
+		}
+	} while(ret != Z_STREAM_END);
+	deflateEnd(&z);
+	size_t compressed_size = z.total_out;
+	compressed_data.resize(compressed_size);
+	UI_write(compressed_size, out, false);
+	out << "IDAT";
+	out.write(reinterpret_cast<char *>(compressed_data.data()), compressed_size);
+	UI_write(0, out, false); // CRC分からん
+	UI_write(0, out, false);
+	out << "IEND";
+	UI_write(0, out, false); // CRC分からん
 	return;
 }
 
