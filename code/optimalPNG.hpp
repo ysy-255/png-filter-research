c6d6a612610d55339f6ce791c9714b0004060bce
ysy-255 <ysy-255@outlook.com>
2024-08-13 22:04:28 +0900

    エントロピーの式変形

--- code/optimalPNG.hpp
+++ code/optimalPNG.hpp
@@ -4,17 +4,25 @@
 #include "PNG.hpp"
 
 float myratePredictor(std::vector<unsigned char> & vec){
-	float result = 0;
+	float H_tilde = 0;
 	unsigned int P[256] = {0};
 	std::vector<unsigned char>::iterator offset = vec.begin();
 	std::vector<unsigned char>::iterator last = vec.end();
 	while(offset != last) ++P[*offset ++];
 	for(int x = 0; x < 256; x++){
 		if(P[x] > 0){
-			result += P[x] * std::log2(P[x]);
+			H_tilde += P[x] * std::log2(P[x]);
 		}
 	}
-	return result;
+	return H_tilde;
 }
 
 #endif
+
+
+/*
+H(S) = - \frac{1}{n} \sum_{X} P(X) \log_2 P(X)\\
+\quad \space H(S) \cdot n^2 - n \log_2n = - \sum_{X} S(X) \log_2 S(X) \quad
+∵P(X) = \frac{S(X)}{n}\\
+\tilde{H}(S) = \sum_{X} S(X) \log_2 S(X)\qquad\,
+*/
