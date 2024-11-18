921155cfd3384a5600b0c57bc11c6a02babde517
ysy-255 <ysy-255@outlook.com>
2024-11-18 22:40:42 +0900

    データセットを調整, これ以降の /csv /result の更新はまとめて最後にコミットします

--- code/dataset.hpp
+++ code/dataset.hpp
@@ -22,10 +22,21 @@ bool dataset_allPNG(void (*funcs[])(PNG_opt &), const uint32_t func_num, std::st
 }
 
 std::vector<std::string> dataset_paths = {
+	// CG画像 スクリーンショット
 	"/mnt/c/test images/afreightdata",
-	"/mnt/c/test images/C00C00",
+		// https://homepages.inf.ed.ac.uk/amos/afreightdata.html [afreightim001.png - afreightim455.png]
 	"/mnt/c/test images/screenshot",
+		// my random 100 screenshots (including various sizes)
+
+	// ピクトアーツさんの画像も使おうかと思っています  https://pictarts.com/
+
+
+
+	// 自然画像(の保存にPNGが使われることはあまりないのでおまけみたいなものかもしれません)
+	"/mnt/c/test images/C00C00",
+		// https://testimages.org/sampling/ -> 8BIT -> RGB -> 1200x1200 -> C00C00 (all)
 	"/mnt/c/test images/X4"
+		// https://data.vision.ee.ethz.ch/cvl/DIV2K/ -> DIV2K/DIV2K_train_LR_bicubic/X4/ [0001x4.png - 0200x4.png]
 };
 
 #endif
